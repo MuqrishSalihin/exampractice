1710622dd51434cb1b1172ea8fd45c316419b36d
MuqrishSalihin <106722917+MuqrishSalihin@users.noreply.github.com>
2025-05-04 21:27:12 +0000

    type

--- 1870_algorithm_codes/recursive.py
+++ 1870_algorithm_codes/recursive.py
@@ -1,3 +1,5 @@
+
+
 def h(n,k):
     if k == 1:
         return n
@@ -10,6 +12,8 @@ def h(n,k):
 print(f"The value of function h is {h(5,3)}")
 
 
+#factorial recursion 
+
 def factorial(n):
     if n == 0: 
         return 1
@@ -17,9 +21,11 @@ def factorial(n):
         return factorial(n-1)*n
     
     
-n = int (input("Calculate factorial:"))  #exxplicit casting
+n = int (input("Calculate factorial:"))  #explicit casting
 print(factorial(n))
 
+#binary search recursion 
+#time complexity = O(log n)
 
 def binary_search(arr,K, left = 0, right = None):
     if right == None:
@@ -39,19 +45,19 @@ array = [1,2,3,4,5,7,45,68,73,82]
 K = 82
 print(f"{K} is located at {binary_search(array,K)}th index")
 
+#Merge sort algorithm
+# Time complexity = O(nlog n)
+
 def merge_sort(arr):
 
     if len(arr) <= 1:
-        return arr
+        return arr  #this is the base case
 
     middle = len(arr) //2 
     left_arr = arr[:middle]
     right_arr = arr[middle:]
-
-    sorted_left = merge_sort(left_arr)
-    sorted_right = merge_sort(right_arr)
-
-    return merge(sorted_left, sorted_right)
+    #all the code above, is basically slicing the array, until each element of the array formed =1
+    return merge(merge_sort(left_arr), merge_sort(right_arr))
     
 def merge(left,right):
     result = []
@@ -74,9 +80,46 @@ def merge(left,right):
 array = [7,2,9,4,3,1,5,8]
 sorted_array = merge_sort(array)
 
+for elements in sorted_array:
+    print(elements)
+
 print(f"The sorted array for {array} is {sorted_array}")
 
 
+#quick sort algorithm 
+#the key keeps moving, according to the array.
+
+def partition (arr, low, high):
+    pivot  = arr[high]
+
+    i = low - 1   #index = -1
+
+    for j in range(low,high):
+        if arr[j] < pivot:
+            i += 1
+            swap(arr, i ,j)
+    swap(arr, i + 1, high)
+    return i + 1
+
+def swap (arr, i, j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp 
+
+def quick_sort(arr, low, high):
+    if low < high:
+        partition_index = partition(arr, low, high)
+
+        #this is where the sorting begins
+        quick_sort(arr, low, partition_index - 1) #sorting below pivot
+        quick_sort(arr, partition_index + 1, high) #sorting above pivot
+
+array = [7,2,9,4,3,1,5,8]
+n = len(array)
+
+quick_sort(array, 0, n-1)
+
+print(array)
 
 
 
